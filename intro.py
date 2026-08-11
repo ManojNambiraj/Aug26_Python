2b7e6e61ec48beb39e1505e8e19c222d59e65810
manojnambiraj <postboxmano@gmail.com>
2026-08-11 21:02:28 +0530

    Python intro to Datatypes

---
+++ intro.py
@@ -0,0 +1,41 @@
+# Output statement:
+
+    # print("Hello")
+
+# End stmt:
+"""
+   print("hello", end="...")
+   print("world", end=", ")
+   print("Good evening")
+"""
+
+# Separate stmt:
+
+    # print("11","01","2026", sep="/")
+
+
+# Variables:
+
+    # age2 = 10
+
+    # print(age2)
+
+# Datatypes:
+
+    # a = 100
+    # a = 100.5
+    # a = 3j
+    # a = '''apple is a fruits,
+    # this fruit color is a red'''
+    # a = True
+    # a = None
+
+# Collection Datatypes:
+
+    # a = ["apple", "banana", "grapes"]
+    # a = ("apple", "banana", "grapes")
+    # a = {"apple", "banana", "grapes"}
+    # a = {"name": "raj", "age": 23}
+
+    # print(a)
+    # print(type(a))
